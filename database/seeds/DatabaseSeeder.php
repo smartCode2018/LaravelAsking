eff7e274ccf1376a2ddeb208dc56d4b416560a0c
smartCode2018 <smartcode.kc@gmail.com>
2019-11-12 11:17:20 +0100

    My First Commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 3)->create()->each(function($u) {
+            $u->questions()
+              ->saveMany(
+                  factory(App\Question::class, rand(1, 5))->make()
+              );
+        });   
+    }
+}
