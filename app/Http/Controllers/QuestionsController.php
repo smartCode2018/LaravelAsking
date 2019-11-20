c6b0262451c00c987ebab563aadc84f5214ca567
smartCode2018 <smartcode.kc@gmail.com>
2019-11-20 13:26:01 +0100

    add deleting question functionalities

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -66,7 +66,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view("questions.edit", compact('question'));
     }
 
     /**
@@ -76,9 +76,10 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+        return redirect('/questions')->with('success', "Your question has been updated");
     }
 
     /**
@@ -89,6 +90,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect('/questions')->with('success', "Your question has been deleted.");
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -32,7 +32,20 @@
                                 </div>
                             </div>
                             <div class="media-body">
-                            <h4 class="mt-0"><a href="{{$question->url}}">{{ $question->title }}</a></h4>
+                                <div class="d-flex align-items-center">
+                                    <h4 class="mt-0"><a href="{{$question->url}}">{{ $question->title }}</a></h4>
+                                    <div class="ml-auto">
+                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure')">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            
                                 <p class="lead">
                                     Asked by
                                     <a href="{{$question->user->url}}">{{$question->user->name}}</a>
