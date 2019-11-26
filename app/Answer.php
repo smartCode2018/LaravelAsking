d2ea9994251f829c57b450caddb39d8e3bf194af
smartCode2018 <smartcode.kc@gmail.com>
2019-11-26 10:21:51 +0100

    updated answers

--- app/Answer.php
+++ app/Answer.php
@@ -17,4 +17,11 @@ class Answer extends Model
     public function getBodyHtmlAttribute(){
         return \Parsedown::instance()->text($this->body);
     }
+
+    public static function boot(){
+        parent::boot();
+        static::created(function ($answer){
+            $answer->question->increment('answers_count');
+        });
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,10 @@ class DatabaseSeeder extends Seeder
             $u->questions()
               ->saveMany(
                   factory(App\Question::class, rand(1, 20))->make()
-              );
+              )
+              ->each(function ($q) {
+                $q->answers()->saveMany(factory(App\Answer::class, rand(1, 5))->make());
+              });
         });   
     }
 }
